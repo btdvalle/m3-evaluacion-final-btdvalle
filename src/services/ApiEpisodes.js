801a583f9496bef8bc5238465fc140be90216d85
btdvalle <btdvalle@gmail.com>
2019-09-25 12:05:19 +0200

    In progress Promise.all

--- src/services/ApiEpisodes.js
+++ src/services/ApiEpisodes.js
@@ -1,11 +1,24 @@
-const getEpisodesNames = episodes => {
-  console.log(episodes);
-  const promises = episodes.map(episode => {
-    return fetch(episode)
-      .then(response => response.json())
-      .then(data => console.log(data));
-    Promises.all(promises).then;
+const getEpisodesNames = totalEpisodes => {
+  const promises = totalEpisodes.map(episodesOfEachCharacter => {
+    return episodesOfEachCharacter.map(episode => {
+      return fetch(episode).then(response => response.json());
+      // .then(data => console.log(data));
+    });
   });
+  Promise.all(promises).then(datas => formatEspisodesData(datas));
 };
 
+const formatEspisodesData = datas => {
+  const episodesOfEachCharacter = datas.map(episodesOfEachCaracter =>
+    console.log(
+      episodesOfEachCaracter.map(promise => {
+        //aquí recorro el array de cada episodio, que contiene un objeto que se llama [[PromiseValue]] y contiene el nombre
+        return promise;
+      })
+    )
+  );
+
+  //este es un array de 20 (personajes), y cada elemento contiene un array con los episodios en los que sale cada personaje
+  return episodesOfEachCharacter;
+};
 export default getEpisodesNames;
